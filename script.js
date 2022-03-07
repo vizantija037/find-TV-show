0e260a98eeff622e6cea7bdb46f39def66268f97
vizantija037 <80783335+vizantija037@users.noreply.github.com>
2022-03-07 11:32:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function toFetch() {
   fetch("https://api.tvmaze.com/schedule/full")
     .then((resp) => resp.json())
     .then((data) => {
-      localStorage.setItem("main", JSON.stringify(data.slice(21, 61)));
+      localStorage.setItem("main", JSON.stringify(data.slice(120, 196)));
     });
 }
 
@@ -57,18 +57,19 @@ function showAll() {
   bannerMain.style.transform = "translateY(0px)";
   document.querySelector(".arrows").style.display = "flex";
   allShowsObject.forEach((show) => {
+    
     if (show._embedded.show.image) {
       output += `
-<div class="card-main" id="${show._embedded.show.id}" style="background-image:url(${show._embedded.show.image.original})" >
-<p class="card-heading">${show._embedded.show.name}</p>
-<a class="card-details" data-id="${show._embedded.show.id}">Show more</a>
+      <div class="card-main" id="${show._embedded.show.id}" style="background-image:url(${show._embedded.show.image.original})" >
+      <p class="card-heading">${show._embedded.show.name}</p>
+      <a class="card-details" data-id="${show._embedded.show.id}">Show more</a>
 
-</div>
-</div>
-`;
+      </div>
+      </div>
+      `;
     }
 
-    content.innerHTML = output;
+  content.innerHTML = output;
   });
 
   let testliste = document.getElementsByClassName("card-details");
@@ -87,9 +88,9 @@ function showSingle(e) {
     let myShow = foundShow._embedded.show;
     let bannerMain = document.querySelector(".banner-main");
     bannerMain.innerHTML = ` 
-  <h1 class="h1-in-banner">${myShow.name}</h1>
-  <button class="join-for-free back-to-all">BACK TO ALL SHOWS</button>
-  `;
+    <h1 class="h1-in-banner">${myShow.name}</h1>
+    <button class="join-for-free back-to-all">BACK TO ALL SHOWS</button>
+    `;
     bannerMain.style.height = "40vh";
     bannerMain.style.transform = "translateY(120px)";
     document.querySelector(".arrows").style.display = "none";
@@ -97,14 +98,15 @@ function showSingle(e) {
     backButton.addEventListener("click", () => {
       showAll();
     });
+    
     let getSummary = foundShow._embedded.show.summary;
     function turncate() {
       if(getSummary == null){
         getSummary = "Sorry, summary is unavailable in this moment";
         return getSummary;   
+        }
+      return getSummary.slice(0, 250)+" ...";
       }
-      return getSummary.slice(0, 200)+" ...";
-    }
     let genresOutput = "";
     function genresLookUp() {
       if (myShow.genres.length > 0) {
@@ -114,8 +116,9 @@ function showSingle(e) {
       }
     }
     genresLookUp();
-    test();
-    async function test() {
+    fetchCast();
+
+    async function fetchCast() {
       try {
         await fetch(`https://api.tvmaze.com/shows/${myShow.id}/cast`)
           .then((resp) => resp.json())
@@ -132,56 +135,84 @@ function showSingle(e) {
           for (let i = 0; i < castObj.length; i++) {
             const person = castObj[i].person
             const character = castObj[i]?.character || {name: ""}
-            const path = person?.image?.original || "./img/notfound.png";
+            const imagePath = person?.image?.original || "./img/notfound.png";
             
             outputList += 
             `
-            <li class="list-item">${person.name}
-            
-              <img class="person-image" src="${path}"/>
+              <li class="list-item">${person.name}
+              <img class="person-image" src="${imagePath}"/>
               <div class="character"> <span>The roll</span><br>${character.name}</div>
               </li>
-              `
-              
+              ` 
           }
-          
           if(!outputList.length){
             outputList += 'Sorry, this information is unavailable in this moment'
-          }
-          
-        
-        
-          
-          console.log(outputList)
+          } 
           return outputList;
         }
-
         let output = `
-  <div class="card-small" id="${myShow.id}" style="background-image:url(${myShow.image.original})"></div>
-  <div class="description-holder">
-    <div class="show-description">${turncate()}</div>
-    <div class="imdb-holder">
-      <a class="imdb-flex" href="http://www.imdb.com/title/${myShow.externals.imdb} 
-      "target="_blank"><img class="imdb-img" src="./img/imdb-logo.png"/></a>
-    <div class="genres">Genre: <span class="span-genres">${genresOutput}</span></div>
-    <div class="language">Language: <span class="span-genres">${myShow.language}</span></div>
-    <div class="premiered">Premiered: <span class="span-genres">${myShow.premiered}</span></div>
-    <div class="website">Ofiicial website: <a class="span-genres" href="${myShow.officialSite}
-    "target="_blank">LINK</a></div>
- 
-
-
-  </div>
-  </div>
-  <div class="cast-all"><span class="cast-title"> Cast</span> <div class="list">${test()}</div>
-  `;
-
+        <div class="card-small" id="${myShow.id}" style="background-image:url(${myShow.image.original})"></div>
+        <div class="description-holder">
+          <div class="show-description">${turncate()}</div>
+          <div class="imdb-holder">
+            <a class="imdb-flex" href="http://www.imdb.com/title/${myShow.externals.imdb} 
+            "target="_blank"><img class="imdb-img" src="./img/imdb-logo.png"/></a>
+            <div class="genres">Genre: <span class="span-genres">${genresOutput}</span></div>
+            <div class="language">Language: <span class="span-genres">${myShow.language}</span></div>
+            <div class="premiered">Premiered: <span class="span-genres">${myShow.premiered}</span></div>
+            <div class="website">Ofiicial website: <a class="span-genres" href="${myShow.officialSite} "target="_blank">LINK</a></div>
+          </div>
+        </div>
+        <div class="cast-all"><span class="cast-title"> Cast</span> <div class="list">${test()}</div>
+        ${episodes()}
+        
+        
+        
+        `
         content.innerHTML = output;
       }
     }
+    function episodes() {
+
+        fetch(`https://api.tvmaze.com/shows/${myShow.id}/episodes`)
+          .then((resp) => resp.json())
+          .then((data) => {
+            localStorage.setItem("episodes", JSON.stringify(data));
+          });
+        let episodesStorage = localStorage.getItem("episodes");
+        let episodesObj = JSON.parse(episodesStorage);
+        
+        
+        let output = `
+        <div class="episodes-all"><span class="cast-title">Episodes</span></div>  
+        <div class="episodes-heading">
+          <div class="episodes-season">Season No.</div>
+          <div class="episodes-number">Episode No.</div>
+          <div class="episodes-name">Name</div>
+          <div class="episodes-date">Date</div>
+
+        </div>    
+        `;
+        episodesObj.forEach(episode=>{
+          output +=
+          `
+          <div class="episode-holder">
+            <div class="episode-season">${episode.season}</div>
+            <div class="episode-number">${episode.number}</div>
+            <div class="episode-name">${episode.name}</div>
+            <div class="episode-date">${episode.airdate}</div>
+
+          </div>
+          `
+        })
+
+
+
+        return output;
+      
+    }
   }
 }
-
 
 
 showAll();
